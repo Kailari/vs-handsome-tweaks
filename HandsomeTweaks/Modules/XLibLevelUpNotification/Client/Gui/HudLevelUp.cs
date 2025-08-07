f9c81d47f4cbe7199eff6f0972a15a9211314a96
Inari Tommiska <inari@jakojaannos.fi>
2025-08-07 16:42:35 +0300

    wip: add keybind hint to level up notification

--- HandsomeTweaks/Modules/XLibLevelUpNotification/Client/Gui/HudLevelUp.cs
+++ HandsomeTweaks/Modules/XLibLevelUpNotification/Client/Gui/HudLevelUp.cs
@@ -141,6 +141,7 @@ public class HudLevelUp : HudElement {
 
 	private readonly AssetLocation _levelUpSfx;
 	private readonly string _labelText;
+	private readonly string _labelHint;
 	private Tween? _animation;
 
 	public HudLevelUp(ICoreClientAPI capi, Skill skill, int level)
@@ -148,16 +149,27 @@ public class HudLevelUp : HudElement {
 	}
 
 	public HudLevelUp(ICoreClientAPI capi, string skill, int level)
-		: this(capi, message: FormatSkillLevelUpMessage(skill, level)) {
+		: this(
+			  capi,
+			  message: FormatSkillLevelUpMessage(skill, level),
+			  hint: FormatOpenSkillUIHint(capi)
+	) {
 	}
 
 	private static string FormatSkillLevelUpMessage(string skill, int level) {
 		return Lang.Get(Guis.TranslationKey("xskills/level-up/label"), skill, level);
 	}
 
-	public HudLevelUp(ICoreClientAPI capi, string message) : base(capi) {
+	private static string FormatOpenSkillUIHint(ICoreClientAPI capi) {
+		var hotkey = capi.Input.GetHotKeyByCode("skilldialoghotkey");
+		var hotkeyString = hotkey.CurrentMapping.ToString();
+		return Lang.Get(Guis.TranslationKey("xskills/level-up/hint"), hotkeyString);
+	}
+
+	public HudLevelUp(ICoreClientAPI capi, string message, string hint) : base(capi) {
 		_levelUpSfx = Assets.Path("sounds/level-up");
 		_labelText = message;
+		_labelHint = hint;
 	}
 
 	internal TextCommandResult OnDebugLevelUp() {
@@ -175,6 +187,24 @@ public class HudLevelUp : HudElement {
 		var labelBounds = ElementBounds.Fixed(0, 0);
 		font.AutoBoxSize(_labelText, labelBounds);
 
+		var separatorFont = CairoFont.WhiteMediumText();
+		separatorFont.FontWeight = Cairo.FontWeight.Bold;
+
+		var separatorText = "________";
+		var separatorBounds = labelBounds.BelowCopy();
+		separatorFont.AutoBoxSize(separatorText, separatorBounds);
+
+		var hintBounds = separatorBounds.BelowCopy();
+		var hintFont = CairoFont.WhiteDetailText();
+		hintFont.Slant = Cairo.FontSlant.Italic;
+		hintFont.Orientation = EnumTextOrientation.Left;
+		hintFont.AutoBoxSize(_labelHint, hintBounds);
+
+		while (separatorBounds.OuterWidth < hintBounds.OuterWidth || separatorBounds.OuterWidth < labelBounds.OuterWidth) {
+			separatorText += "_";
+			separatorFont.AutoBoxSize(separatorText, separatorBounds);
+		}
+
 		// Position horizontally centered and vertically at 25% from the top of the screen
 		var screenHeight = capi.Gui.WindowBounds.InnerHeight / RuntimeEnv.GUIScale;
 		var offsetY = screenHeight / 4.0f;
@@ -191,15 +221,27 @@ public class HudLevelUp : HudElement {
 			.AddContainer(ElementBounds.Fill)
 			.BeginChildElements()
 			.AddScrollingText(_labelText, font, labelBounds, "levelup-notification-animation-text")
+			.AddScrollingText(separatorText, font, labelBounds, "levelup-notification-animation-separator")
+			.AddScrollingText(_labelHint, font, hintBounds, "levelup-notification-animation-hint")
 			.EndChildElements()
 			.Compose();
 
 		var label = SingleComposer.GetScrollingText("levelup-notification-animation-text");
+		var separator = SingleComposer.GetScrollingText("levelup-notification-animation-separator");
+		var hint = SingleComposer.GetScrollingText("levelup-notification-animation-hint");
 		_animation = new Tween()
+			.TweenProperty(v => separator.VisibleFactor = v, 0.0, 1.0, (float)FADE_IN_DURATION)
+			.Wait(0.1f)
 			.TweenProperty(v => label.VisibleFactor = v, 0.0, 1.0, (float)FADE_IN_DURATION)
+			.Wait(0.1f)
+			.TweenProperty(v => hint.VisibleFactor = v, 0.0, 1.0, (float)FADE_IN_DURATION)
 			.WaitAll()
 			.Wait((float)FADE_DELAY)
-			.TweenProperty(v => label.Font.Color[3] = v, 1.0, 0.0, (float)FADE_OUT_DURATION);
+			.TweenProperty(v => label.Font.Color[3] = v, 1.0, 0.0, (float)FADE_OUT_DURATION)
+			.Wait(0.2f)
+			.TweenProperty(v => hint.Font.Color[3] = v, 1.0, 0.0, (float)FADE_OUT_DURATION - 0.2f)
+			.Wait(0.2f)
+			.TweenProperty(v => separator.Font.Color[3] = v, 1.0, 0.0, (float)FADE_OUT_DURATION - 0.4f);
 
 		_animation.Finished += () => TryClose();
 		_animation.SeekTo(0.0f);
